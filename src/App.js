6abad45f0fc04ca21e8fcd98283a2929347ba7d5
DevMizeKR <hcg082743@gmail.com>
2025-12-22 20:57:35 +0900

    [Chore] Added React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="main-nav">
-        <div className="internal-links">
-          <img className="profile-image-nav" alt="DevMizeKR" src="images/profile.jpg" />
-          asdf
-        </div>
+      <header>
+        <nav className="nav-main">
+          <div>
+            <img className="nav-image-profile" alt="DevMizeKR" src="images/profile.jpg" />
+            <div className="nav-text">Main</div>
+            <div className="nav-text">Blog</div>
+          </div>
+          <div>
+            <img className="nav-image-profile" alt="DevMizeKR" src="images/profile.jpg" />
+            <div className="nav-text">Main</div>
+            <div className="nav-text">Blog</div>
+          </div>
+        </nav>
       </header>
     </div>
   );
