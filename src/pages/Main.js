b349dcda5c068b8c0d41a5fc5cf0c2506e374d5c
DevMizeKR <hcg082743@gmail.com>
2025-12-25 20:56:39 +0900

    [Feat] Constructing Main Bage Profile Card

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,22 @@
+import "./Main.css"
+
 function Main() {
-  return ( "asdf"
+  return ( 
+    <div className="">
+      <aside className="side-card">
+        <div className="">
+          <img
+            src="images/profile.jpg"
+            alt="DevMizeKR"
+            className="side-image-profile"
+          />
+        </div>
+        <h1 className="side-text-profile">Mize</h1>
+        <p className="">Game Developer @ KR</p>
+        <div className="">
+        </div>
+      </aside>
+    </div>
   );
 }
 
