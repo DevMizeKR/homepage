5be21841971c4dd05100243f7feb3f0579d39f5a
DevMizeKR <hcg082743@gmail.com>
2025-12-29 14:01:16 +0000

    [Design] Added Project Logo Files

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -15,16 +15,16 @@ function Main() {
         <p className="side-text-description">Game Developer @ KR</p>
         <div className="side-link">
           <a href="https://www.instagram.com/hwang_c_k_/" target="_blank" rel="noopener noreferrer">
-            <img src="/icons/logo_instagram.svg" alt="Instagram" className="side-image-link" />
+            <img src="/icons/sns/logo_instagram.svg" alt="Instagram" className="side-image-link" />
           </a>
           <a href="https://www.linkedin.com/in/%EC%B0%AC%EA%B8%B0-%ED%99%A9-6a75062ba/" target="_blank" rel="noopener noreferrer">
-            <img src="/icons/logo_linkedin.svg" alt="LinkedIn" className="side-image-link" />
+            <img src="/icons/sns/logo_linkedin.svg" alt="LinkedIn" className="side-image-link" />
           </a>
           <a href="https://github.com/DevMizeKR" target="_blank" rel="noopener noreferrer">
-            <img src="/icons/logo_github.svg" alt="GitHub" className="side-image-link" />
+            <img src="/icons/sns/logo_github.svg" alt="GitHub" className="side-image-link" />
           </a>
           <a href="mailto:hwangck0827@gmail.com" target="_blank" rel="noopener noreferrer">
-            <img src="/icons/logo_gmail.svg" alt="Gmail" className="side-image-link" />
+            <img src="/icons/sns/logo_gmail.svg" alt="Gmail" className="side-image-link" />
           </a>
         </div>
       </div>
